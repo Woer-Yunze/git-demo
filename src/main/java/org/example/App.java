05f7e3236b1ea3e904829fa1cb0891ae192c6bc7
Woer_Yunze <woer_yushu@163.com>
2021-07-31 00:53:23 +0800

    主干操作

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "分支操作" );
+        System.out.println( "主干操作" );
     }
 }
